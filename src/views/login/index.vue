d4b0ad0766d1bae90d13bbf1453eb882fac68b78
Diamond-sjh <986694880@qq.com>
2019-08-22 19:44:17 +0800

    完成登录页面的form表单部分

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,6 +2,25 @@
   <div class="container">
     <el-card class="box-card">
       <img src="../../assets/images/logo_index.png" alt />
+      <el-form ref="form" :model="loginForm">
+        <el-form-item>
+          <el-input placeholder="请输入手机号" v-model="loginForm.mobile"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            placeholder="请输入验证码"
+            v-model="loginForm.code"
+            style="width:70%;margin-right:25px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value='true'>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%;">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -37,7 +56,7 @@ export default {
     img {
       display: block;
       width: 60%;
-      margin: 0 auto;
+      margin: 0 auto 25px;
     }
   }
 }
