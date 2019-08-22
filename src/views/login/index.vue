76dd633eb45711caba9cfc076780c1e324f2acd2
Diamond-sjh <986694880@qq.com>
2019-08-22 19:26:34 +0800

    引入Element模块&路由文件配置&完成登录页面基本结构

---
+++ src/views/login/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <el-card class="box-card">
+      <img src="../../assets/images/logo_index.png" alt />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  .box-card {
+    width: 500px;
+    height: 400px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 60%;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
