be035de88c956ef9db7af81b834e2ec37a37d215
Diamond-sjh <986694880@qq.com>
2019-08-23 17:19:13 +0800

    登录功能完成

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 
 import router from '@/routers'
 
+import Axios from 'axios'
+
 // 引入element-ui模块和CSS文件
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUi)
+Axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/'
+Vue.prototype.$http = Axios
 
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,15 +2,15 @@
   <div class="container">
     <el-card class="box-card">
       <img src="../../assets/images/logo_index.png" alt />
-      <el-form ref="form" :model="loginForm">
-        <el-form-item>
+      <el-form ref="loginForm" :rules="rules" status-icon :model="loginForm">
+        <el-form-item prop="mobile">
           <el-input placeholder="请输入手机号" v-model="loginForm.mobile"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input
             placeholder="请输入验证码"
             v-model="loginForm.code"
-            style="width:70%;margin-right:25px"
+            style="width:65%;margin-right:30px"
           ></el-input>
           <el-button>发送验证码</el-button>
         </el-form-item>
@@ -18,7 +18,7 @@
           <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%;">登录</el-button>
+          <el-button type="primary" @click="login()" style="width:100%;">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -28,13 +28,48 @@
 <script>
 export default {
   data () {
+    // 定义验证手机号码格式的规则
+    const checkMobile = (rule, value, callback) => {
+      if (!/^1[3-9][0-9]{9}$/.test(value)) {
+        return callback(new Error('手机号码格式错误'))
+      }
+      callback()
+    }
     return {
       loginForm: {
-        mobile: '',
-        code: ''
+        mobile: '18311111111',
+        code: '246810'
+      },
+      // 根据element示例定义校验规则
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          // 自定义手机号码的规则验证
+          { validator: checkMobile, trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位数字', trigger: 'blur' }
+        ]
       }
     }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          this.$http.post('/mp/v1_0/authorizations', this.loginForm)
+            .then((result) => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message.error('手机号或者验证码错误')
+            })
+        }
+      })
+    }
   }
+
 }
 </script>
 
@@ -47,8 +82,8 @@ export default {
   left: 0;
   background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
   .box-card {
-    width: 500px;
-    height: 400px;
+    width: 450px;
+    height: 380px;
     position: absolute;
     top: 50%;
     left: 50%;
