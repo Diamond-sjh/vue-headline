95849fa056ce7e0d57cde7f30296bcb75dc9fa4e
Diamond-sjh <986694880@qq.com>
2019-08-23 20:03:50 +0800

    侧边栏和顶部内容基本实现

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,60 @@
 <template>
   <el-container class="container">
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside width="200px">
+      <div class="pic-box"></div>
+      <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        background-color="#002033"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <span class="el-icon-s-fold icon"></span>
+        <span class="title">江苏传智播客科技教育有限公司</span>
+        <el-dropdown>
+          <span class="el-dropdown-link my-dropdown">
+          <img src="../../assets/images/avatar.jpg" alt="">
+            用户名
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -13,17 +65,45 @@ export default {}
 </script>
 
 <style lang="less" scoped>
-.container{
+.container {
   width: 100%;
   height: 100%;
   position: absolute;
   top: 0;
   left: 0;
-  .el-aside{
+  .el-aside {
     background: #002033;
+    .pic-box {
+      width: 100%;
+      height: 60px;
+      background: #002244 url(../../assets/images/logo_admin.png) no-repeat
+        center / 140px auto;
+    }
+    .el-menu {
+      border-right: none;
+    }
   }
   .el-header {
     border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      font-size: 24px;
+      vertical-align: middle;
+      margin-right: 10px;
+    }
+    .title {
+      vertical-align: middle;
+    }
+    .el-dropdown {
+      float: right;
+      img {
+        width: 30px;
+        vertical-align: middle;
+      }
+      .my-dropdown {
+        cursor: pointer;
+      }
+    }
   }
 }
 </style>
