3fe5f0dfa1ad8d76ab4a66c6b491aac16826a3fc
Diamond-sjh <986694880@qq.com>
2019-08-23 17:41:54 +0800

    主页路由配置&主页框架

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
-    { path: '/login', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    { path: '/', name: 'home', component: Home }
   ]
 })
---
+++ src/views/home/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container class="container">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.container{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  .el-aside{
+    background: #002033;
+  }
+  .el-header {
+    border-bottom: 1px solid #ddd;
+  }
+}
+</style>
